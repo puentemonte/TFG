ad3d389921e319b1f5f1a4c180aace0ee5d2aa8c
Elena Novillo <elennovi@ucm.es>
2022-11-19 18:58:35 +0100

    Modificados los colores, fuente y añadida la fixed-navbar

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     </head>
 
     <body>
-    <header class="p-3 mb-3 custom-color-header">
+    <header class="p-3 mb-3 custom-color-header fixed-top">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-light text-decoration-none">
