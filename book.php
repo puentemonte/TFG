6257ea9a013630bc82305127ff85e0cc9455e079
Elena Novillo <elennovi@ucm.es>
2022-12-01 18:57:00 +0100

    Book class

---
+++ book.php
@@ -0,0 +1,88 @@
+<?php
+
+class Book {
+    // book isbn
+    private $isbn;
+
+    // queries
+	private $conn;
+
+    public function __construct($conn, $isbn){
+		$this->conn = $conn;
+        $this->isbn = $isbn;      
+	}
+
+
+
+    public function get_full_info(){
+		// get the data from books table
+        $query = mysqli_query($this->conn, "SELECT * FROM books WHERE isbn='$isbn'");
+        $book_data = mysqli_fetch_array($query);
+
+        // get reviews from books-users table
+        $reviews = get_reviews();
+        
+        // get average rating
+        $avg_rating = get_avg_rating($reviews);       
+
+        // get rating distribution
+        $distribution_ratings = get_distribution_ratings($reviews);
+
+        $ret = array("title" => $book_data['title'], 
+                    "author" => $book_data['author'],
+                    "editorial" => $book_data['editorial'],
+                    "translator" => $book_data['author'],
+                    "pages" => $book_data['author'],
+                    "releaseDate" => $book_data['author'],
+                    "genres" => $book_data['author'],
+                    "synopsis" => $book_data['author'],
+                    "reviews" => $reviews, 
+                    "avg_rating" => $avg_rating,
+                    "distribution_ratings" => $distribution_ratings);
+        
+        return $ret;
+	}
+
+    private function get_reviews() {
+        $query = mysqli_query($this->conn, "SELECT * FROM books WHERE isbn='$isbn'")
+        $reviews = mysqli_fetch_array($query);
+        return $reviews;
+    }
+
+    private function get_avg_rating($reviews){
+        $total = 0;
+        $acc = 0;
+        foreach($reviews as $key => $value) {
+            if ($value['rating'] != NULL){
+                $acc += $value['rating'];
+                ++$total;
+            }
+        }
+
+        // calculating the avg
+        $avg = acc / total;
+        return round($avg, 1); // round to the first decimal 
+    }
+
+    private function get_distribution_ratings($reviews){
+        // 0.5 -> 1, 1 -> 2, 1.5 -> 3...
+        $ret = array(1 => 0,
+                    2 => 0,
+                    3 => 0,
+                    4 => 0,
+                    5 => 0,
+                    6 => 0,
+                    7 => 0,
+                    8 => 0,
+                    9 => 0,
+                    10 => 0,);
+
+        foreach($reviews as $key => $value) {
+            if ($value['rating'] != NULL){
+                $rating_10 = round($value * 2, 0);
+                ++$ret[$rating_10];
+            }
+        }
+        return ret;
+    }
+}
