c12797aad99e59b7bc7a328f1c31f55c48397029
Erik <estizubi@ucm.es>
2022-12-26 19:29:20 +0100

    fixed settings alignment

--- delete-usr.php
+++ delete-usr.php
@@ -2,69 +2,58 @@
     include_once 'header.php';
 ?>
 
-<div class="row">
-    <div class="col-md-auto">
-        <div class="settings-menu">
-            <div class='row'>
-            <a class="btn sidebar-custom" href="settings.php"><b>Actualizar perfil</b></a>
-            </div>
-            <div class='row'>
-                <a class="btn sidebar-custom" href="change-pwd.php"><b>Cambiar contraseña</b></a>
-            </div>
-            <div class='row'>
-                <a class="btn custom-color-sidebar mb-sidebar-custom" aria-current="page" href="delete-usr.php"><b>Eliminar cuenta</b></a>
-            </div>
-        </div>   
+<div class="settings-btn">
+    <div class="center">
+        <a class="btn sidebar-custom" href="settings.php"><b>Actualizar perfil</b></a>
+        <a class="btn sidebar-custom" href="change-pwd.php"><b>Cambiar contraseña</b></a>
+        <a class="btn custom-color-sidebar mb-sidebar-custom" aria-current="page" href="delete-usr.php"><b>Eliminar cuenta</b></a>
     </div>
-
-    <div class="col-md-auto">
-        <div class="form-signin w-100 m-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-            <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-            </symbol>
-            </svg>
-            <?php
-            if (isset($_GET["error"])) {
-                if ($_GET["error"] == "emptyinput"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    Rellena todos los campos
-                                </div>
-                        </div>";
-                }
-                else if ($_GET["error"] == "wrongpwd"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    La contraseña actual es incorrecta
-                                </div>
-                        </div>";
-                }
-            }
-            ?>
-            <form class="delete-usr-form" action="includes/deleteusr_inc.php" method="post" novalidate>
-                <div class="mt-custom">
-                    <h2 class="h3 mb-3 fw-normal">Eliminar cuenta</h2>
-                </div>
-                
-                <div class="form-floating">
-                    <input type="password" class="form-control" name="pwd" placeholder="Contraseña actual">
-                    <label for="floatingPassword" class='floating-input'>Contraseña actual</label>
-                </div>
-                <button type="submit" name="submit" class="w-100 btn btn-lg custom-color-button" onclick="clicked(event)">Eliminar</button>
-            </form>
-
-            <script>
-                function clicked(e) {
-                    if(!confirm('Estas seguro de eliminar tu cuenta?')) {
-                        e.preventDefault();
-                    }
-                }
-            </script>
+</div>
+<div class="form-signin w-100 m-auto text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+    </symbol>
+    </svg>
+    <?php
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            Rellena todos los campos
+                        </div>
+                </div>";
+        }
+        else if ($_GET["error"] == "wrongpwd"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            La contraseña actual es incorrecta
+                        </div>
+                </div>";
+        }
+    }
+    ?>
+    <form class="delete-usr-form" action="includes/deleteusr_inc.php" method="post" novalidate>
+        <div class="mt-custom">
+            <h2 class="h3 mb-3 fw-normal">Eliminar cuenta</h2>
         </div>
-    </div>
+        
+        <div class="form-floating">
+            <input type="password" class="form-control" name="pwd" placeholder="Contraseña actual">
+            <label for="floatingPassword" class='floating-input'>Contraseña actual</label>
+        </div>
+        <button type="submit" name="submit" class="w-100 btn btn-lg custom-color-button" onclick="clicked(event)">Eliminar</button>
+    </form>
+
+    <script>
+        function clicked(e) {
+            if(!confirm('Estas seguro de eliminar tu cuenta?')) {
+                e.preventDefault();
+            }
+        }
+    </script>
 </div>
 
 <?php
--- settings.php
+++ settings.php
@@ -8,106 +8,96 @@
     $pronouns = $_SESSION["pronouns"];
 ?>
 
-<div class="row">
-    <div class="col-md-auto">
-        <div class="settings-menu">
-            <div class='row'>
-            <a class="btn custom-color-sidebar mb-sidebar-custom" aria-current="page" href="settings.php"><b>Actualizar perfil</b></a>
-            </div>
-            <div class='row'>
-                <a class="btn sidebar-custom" href="change-pwd.php"><b>Cambiar contraseña</b></a>
-            </div>
-            <div class='row'>
-                <a class="btn sidebar-custom" href="delete-usr.php"><b>Eliminar cuenta</b></a>
-            </div>
-        </div>   
+<div class="settings-btn">
+    <div class="center">
+        <a class="btn custom-color-sidebar mb-sidebar-custom" aria-current="page" href="settings.php"><b>Actualizar perfil</b></a>  
+        <a class="btn sidebar-custom" href="change-pwd.php"><b>Cambiar contraseña</b></a>
+        <a class="btn sidebar-custom" href="delete-usr.php"><b>Eliminar cuenta</b></a>
     </div>
-    <div class="col-md-auto">
-        <div class="form-signin w-100 m-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
-                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                </symbol>
-            </svg>
-            <?php
-            if (isset($_GET["error"])) {
-                if ($_GET["error"] == "emptyinput"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    Rellena todos los campos
-                                </div>
-                        </div>";
-                }
-                else if ($_GET["error"] == "invalidemail"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    El email introducido no es válido
-                                </div>
-                        </div>";
-                }
-                else if ($_GET["error"] == "usrnametaken"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    El nombre de usuario introducido se encuentra en uso
-                                </div>
-                        </div>";
-                }
-                else if ($_GET["error"] == "emailtaken"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    El email introducido se encuentra en uso
-                                </div>
-                        </div>";
-                }
-                else if ($_GET["error"] == "stmtfailed"){
-                    echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
-                                <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
-                                <div>
-                                    Algo ha salido mal, inténtalo más tarde
-                                </div>
-                        </div>";
-                }
-            }
-            ?>
-            <form action="includes/changedata_inc.php" method="post" novalidate>
-                <div class="mt-custom">
-                    <h2 class="h3 mb-3 fw-normal">Actualizar perfil</h2>
-                </div>
-                <div class="form-floating">
-                    <input type="text" class="form-control" name="fname" placeholder="Nombre" value="<?php
-                    echo ($fname != "NULL") ? $fname : "";?>">
-                    <label for="floatingInput" class='floating-input'>Nombre</label>
-                </div>
-                <div class="form-floating">
-                    <input type="text" class="form-control" name="surname" placeholder="Apellidos" value="<?php
-                    echo ($surname != "NULL") ? $surname : "";?>">
-                    <label for="floatingInput" class='floating-input'>Apellidos</label>
-                </div>
-                <div class="form-floating">
-                    <input type="text" class="form-control" name="uid" placeholder="Nombre de usuario" value="<?php echo $username ?>">
-                    <label for="floatingInput" class='floating-input'>Nombre de usuario</label>
-                </div>
-                <div class="form-floating">
-                    <input class="form-control" name="email" placeholder="nombre@ejemplo.es" value="<?php echo $email?>">
-                    <label for="floatingInput" class='floating-input'>Correo electrónico</label>
-                </div>
-                <div class="form-floating">
-                    <select class="custom-select my-1 mr-sm-2 form-control" name = "pronouns" selected="<?php echo $pronouns?>">
-                        <option value="No seleccionado" <?php  if ($pronouns === "No seleccionado"){ echo "selected";}?>>No seleccionado</option>
-                        <option value="Él" <?php  if ($pronouns === "Él"){ echo "selected";}?>>Él</option>
-                        <option value="Ella" <?php if ($pronouns === "Ella"){ echo "selected";}?>>Ella</option>
-                        <option value="Elle"<?php if ($pronouns === "Elle"){ echo "selected";}?>>Elle</option>
-                    </select>
-                    <label for="floatingInput">Pronombres</label>
-                </div>
-                <button class="w-100 btn btn-lg custom-color-button mb-custom" name="submit" type="submit">Actualizar</button>
-            </form>
+</div>
+<div class="form-signin  w-100 m-auto  text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </symbol>
+    </svg>
+    <?php
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            Rellena todos los campos
+                        </div>
+                </div>";
+        }
+        else if ($_GET["error"] == "invalidemail"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            El email introducido no es válido
+                        </div>
+                </div>";
+        }
+        else if ($_GET["error"] == "usrnametaken"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            El nombre de usuario introducido se encuentra en uso
+                        </div>
+                </div>";
+        }
+        else if ($_GET["error"] == "emailtaken"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            El email introducido se encuentra en uso
+                        </div>
+                </div>";
+        }
+        else if ($_GET["error"] == "stmtfailed"){
+            echo "<div class='mt-custom alert alert-danger d-flex align-items-center' role='alert'>
+                        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+                        <div>
+                            Algo ha salido mal, inténtalo más tarde
+                        </div>
+                </div>";
+        }
+    }
+    ?>
+    <form action="includes/changedata_inc.php" method="post" novalidate>
+        <div class="mt-custom">
+            <h2 class="h3 mb-3 fw-normal">Actualizar perfil</h2>
         </div>
-    </div>
+        <div class="form-floating">
+            <input type="text" class="form-control" name="fname" placeholder="Nombre" value="<?php
+            echo ($fname != "NULL") ? $fname : "";?>">
+            <label for="floatingInput" class='floating-input'>Nombre</label>
+        </div>
+        <div class="form-floating">
+            <input type="text" class="form-control" name="surname" placeholder="Apellidos" value="<?php
+            echo ($surname != "NULL") ? $surname : "";?>">
+            <label for="floatingInput" class='floating-input'>Apellidos</label>
+        </div>
+        <div class="form-floating">
+            <input type="text" class="form-control" name="uid" placeholder="Nombre de usuario" value="<?php echo $username ?>">
+            <label for="floatingInput" class='floating-input'>Nombre de usuario</label>
+        </div>
+        <div class="form-floating">
+            <input class="form-control" name="email" placeholder="nombre@ejemplo.es" value="<?php echo $email?>">
+            <label for="floatingInput" class='floating-input'>Correo electrónico</label>
+        </div>
+        <div class="form-floating">
+            <select class="custom-select my-1 mr-sm-2 form-control" name = "pronouns" selected="<?php echo $pronouns?>">
+                <option value="No seleccionado" <?php  if ($pronouns === "No seleccionado"){ echo "selected";}?>>No seleccionado</option>
+                <option value="Él" <?php  if ($pronouns === "Él"){ echo "selected";}?>>Él</option>
+                <option value="Ella" <?php if ($pronouns === "Ella"){ echo "selected";}?>>Ella</option>
+                <option value="Elle"<?php if ($pronouns === "Elle"){ echo "selected";}?>>Elle</option>
+            </select>
+            <label for="floatingInput">Pronombres</label>
+        </div>
+        <button class="w-100 btn btn-lg custom-color-button mb-custom" name="submit" type="submit">Actualizar</button>
+    </form>
 </div>
 
 <?php
